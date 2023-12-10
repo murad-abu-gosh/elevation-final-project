529d9a86833b31c35f2418e61481c9f020850f7f
khaledidk <khaledidkidk@gmail.com>
2023-12-10 16:53:11 +0200

    logout with API + new user

--- tests/logout-component -tests.spec.ts
+++ tests/logout-component -tests.spec.ts
@@ -1,59 +1,61 @@
-// import test, { Browser, BrowserContext, Page, expect } from "@playwright/test";
-// import { Launcher } from "../src/Infra/Launcher";
-// import { ApiClient } from "../src/Infra/ApiClient";
-// import { LoginComponent } from "../src/Logic/POM/LoginComponent";
-//
-//
-// test.describe("implement the log out tests stream ", () => {
-//
-//     let browser: Browser;
-//     let context: BrowserContext;
-//     let page: Page;
-//     let launcher: Launcher;
-//     let LogOutWithApi: ApiClient;
-//     let loginPage: LoginComponent;
-//
-//     test.beforeAll(async () => {
-//
-//         launcher = new Launcher()
-//         browser = await launcher.launchBrowser()
-//     });
-//     test.beforeEach(async () => {
-//         page = await browser.newPage();
-//         context = await launcher.NewContext()
-//         // page = await launcher.NewPage()
-//         LogOutWithApi = new ApiClient();
-//         loginPage = new LoginComponent(page)
-//         await page.goto(LoginComponent.url);
-//
-//     });
-//     test.afterEach(async () => {
-//
-//         // const name = await loginPage.getProfileNameNoWait();
-//         // if (name != PROFILE_NAME) {
-//         //     await loginPage.fullLoginFlow(LOGIN_EMAIL, LOGIN_PASSWORD)
-//         // }
-//         await page.close();
-//     });
-//     test.afterAll(async () => {
-//         await browser.close();
-//     });
-//
-//     test("check logout feature", async ({ request }) => {
-//
-//         LogOutWithApi.logOutApi(request)
-//         await page.reload()
-//
-//         const status = await loginPage.getProfileNameNoWait()
-//
-//         expect(status).toEqual(null)
-//
-//
-//
-//
-//     })
-//
-//
-//
-//
-// })
+import test, { Browser, BrowserContext, Page, expect } from "@playwright/test";
+import { Launcher } from "../src/Infra/Launcher";
+import { ApiClient } from "../src/Infra/ApiClient";
+import { LoginComponent } from "../src/Logic/POM/LoginComponent";
+import { LOGIN_EMAIL, LOGIN_EMAIL2, LOGIN_PASSWORD, LOGIN_PASSWORD2 } from "../terminal-x-config";
+
+const authFile = 'playwright/.auth/user2.json';
+test.describe("implement the log out tests stream ", () => {
+
+    let browser: Browser;
+    let context: BrowserContext;
+    let page: Page;
+    let launcher: Launcher;
+    let LogOutWithApi: ApiClient;
+    let loginPage: LoginComponent;
+    let LoginWithApi : ApiClient;
+    test.use({storageState: {cookies: [], origins: []}});
+    test.beforeAll(async () => {
+
+     launcher = new Launcher()
+     browser = await launcher.launchBrowser()
+    });
+    test.beforeEach(async ({request }) => {
+        LogOutWithApi = new ApiClient();
+        LoginWithApi = new ApiClient();
+        const res = await LoginWithApi.loginApi(request,LOGIN_EMAIL2 ,LOGIN_PASSWORD2 );
+        const state = await request.storageState();
+        context = await browser.newContext({ storageState: state });
+        page = await context.newPage();
+        await page.goto(LoginComponent.url);
+        loginPage = new LoginComponent(page)
+    
+
+
+
+    });
+    test.afterEach(async () => {
+
+        
+        await page.close();
+    });
+    test.afterAll(async () => {
+        await browser.close();
+    });
+
+    test("check logout feature", async ({ request }) => {
+      
+        LogOutWithApi.logOutApi(request)
+        await page.reload()
+        const status = await loginPage.getProfileNameNoWait()
+        expect(status).toEqual(null)
+
+
+
+
+    })
+
+
+
+
+})
